20aec636e39f838343e6366a703bcfc9fb87e239
SulavChhetri <sulavthapachhetri@gmail.com>
2022-11-07 15:41:25 +0545

    final

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask,render_template,request
-import redis
+import redis,time
 from tasks import count_words
 from rq import Queue
-
 r= redis.Redis(host='localhost',port=6379,db=0)
 q = Queue(connection=r)
+
+
+final_dict = dict()
+final_list = list()
 
 
 app = Flask(__name__)
@@ -19,11 +22,24 @@ def add_task():
     message = None
     if request.method == "POST":
         url = request.form['url']
-        tasks = q.enqueue(count_words,url)
-        jobs = q.jobs
-        q_length = len(q)
-        message = f"The result is {tasks} and the jobs queued are {q_length}"
+        if url:
+            task = q.enqueue(count_words,url)
+            job_ids = task.key[7:]
+            r.hset('key_container',url,job_ids)
+            jobs = q.jobs
+            q_length = len(q)
+            message = f"The result is {task} and the jobs queued are {q_length}"
     return render_template("add_task.html",message= message, jobs = jobs)
+
+@app.route('/getresult')
+def get_result():
+    middle_dict = r.hgetall('key_container')
+    for keys in middle_dict.keys():
+        job_id =middle_dict[keys]
+        job_id = middle_dict[keys].decode()
+        result_data = q.fetch_job(job_id)
+        final_dict[keys.decode()]=result_data.result
+    return render_template("scraperesult.html",result = final_dict)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- tasks.py
+++ tasks.py
@@ -4,4 +4,4 @@ def count_words(url):
     response = requests.get(url).text
     return len(response.split())
 
-# print(count_words('http://nvie.com'))
+# print(count_words('https://www.cricbuzz.com'))
